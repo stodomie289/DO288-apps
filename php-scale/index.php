a6139e6cdf27833ecdd1f7b0f90a5c159ecd9513
root <eric.stodomingo.dlr@gmail.com>
2023-09-03 18:35:08 +0800

    eric rhca

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "This is version 2 of the app. ERIC Sto Domingo RHCA . I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This is version 2 of the app. ERIC Sto Domingo RHCA CCNP version 2.0 . I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
